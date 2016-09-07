479b94e08bbc00b3ecbc09f4901cb7e6ef72314c
tal <tal@soluto.com>
2016-09-07 23:57:45 +0300

    updates

--- mm2-messages.js
+++ mm2-messages.js
@@ -4,16 +4,34 @@ Module.register("mm2-messages",{
         text: "work damn you"
     },
     start: function(){
-        //this.sendSocketNotification('TALTALTAL', {bla:'bla'});
+        this.sendSocketNotification('PING',{});
+    },
+    getScripts: function(){
+        return ['moment.js'];
     },
     // Override dom generator.
     getDom: function() {
         var wrapper = document.createElement("div");
-        wrapper.innerHTML = this.config.text;
+        if (!this.messages || this.messages.length == 0) return wrapper;
+
+        console.log(this.messages);
+        var message = this.messages.pop();
+        var displayTime = moment(message.send).fromNow();
+        wrapper.innerHTML =
+            `<div >
+                <span class="medium bright">${message.from}:${message.text}</span></br>
+                <span class="light small dimmed">${displayTime}</span>
+            </div>`;
+
         return wrapper;
     },
     socketNotificationReceived: function(notification, payload) {
-        Log.log('TALLLLLLLLLLLLL');
+        console.log(notification)
+        if (notification === "MESSAGES"){
+            console.log(payload);
+            this.messages = payload;
+            this.updateDom(0);
+        }
 
     }
 });
--- node_helper.js
+++ node_helper.js
@@ -1,31 +1,32 @@
 'use strict';
 const NodeHelper = require('node_helper');
-//const firebase = require("firebase");
-//var config = {
-//    apiKey: "AIzaSyC_LceL758JO5BCOP8olveLzBtEhZAK01c",
-//    authDomain: "magic-mirror-66db6.firebaseapp.com",
-//    databaseURL: "https://magic-mirror-66db6.firebaseio.com",
-//    storageBucket: "magic-mirror-66db6.appspot.com"
-//};
-//firebase.initializeApp(config);
+const firebase = require("firebase");
 
-
-console.log('dskhgalksghlksadghsd');
 
 module.exports = NodeHelper.create({
     init:function(){
+        var config = {
+            apiKey: "AIzaSyC_LceL758JO5BCOP8olveLzBtEhZAK01c",
+            authDomain: "magic-mirror-66db6.firebaseapp.com",
+            databaseURL: "https://magic-mirror-66db6.firebaseio.com",
+            storageBucket: "magic-mirror-66db6.appspot.com"
+        };
+        firebase.initializeApp(config);
 
     },
     start: function(){
+        var self =this;
+        self.sendSocketNotification('PONG',{});
 
-        //this.sendSocketNotification('LOG',{
-        //    message:message,
-        //    data:data
-        //})
-            //firebase.database().ref('/users/tal/messages').once('value').then(function(messagesSnapshot){
-            //    var messages =messagesSnapshot.val();
-            //    self.sendSocketNotification('MESSAGES',messages);
-            //})
+        firebase.database().ref('/users/asaf/messages').once('value').then(function(messagesSnapshot){
+            console.log('here');
+            if (!messagesSnapshot.exists()) return;
+            console.log('here 2');
+            const messagesByKey =messagesSnapshot.val();
+            console.log(messagesByKey);
+            const messages = Object.keys(messages).map(x=> messagesByKey[x]);
+            self.sendSocketNotification('MESSAGES',messagesByKey);
+        })
 
     }
     //// Subclass socketNotificationReceived received.
